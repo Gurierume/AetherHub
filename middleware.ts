c31b1e8e91cc614d8731acb207a84c7071185452
Gurierume <pseudozin@gmail.com>
2026-02-15 09:01:50 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,8 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Pula arquivos internos do Next.js e arquivos estáticos
+    // Isso garante que o middleware não quebre em arquivos estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Sempre executa para rotas de API
     '/(api|trpc)(.*)',
   ],
 };
